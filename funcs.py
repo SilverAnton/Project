c736e686f18a8eafeac61b05b052a1f98f773574
SilverAnton <cu4hawk@gmail.com>
2024-01-24 14:15:01 +0700

    extra add subtraction foo

--- funcs.py
+++ funcs.py
@@ -1,13 +1,20 @@
-def calc(num, symbol):
+def calc_add(num, symbol):
+    '''функция прибавляет к числу 10, нужно выбрать число и знак +, в качестве аргументов'''
     if symbol == '+':
         num_add = num + 10
         return num_add
-    #elif symbol == '-':
-        #num_subst = num - 10
-        #return num_subst
     else:
         print('ой')
 
 
-item = calc(50, '+')
+def calc_subst(num, symbol):
+    '''функция отнимает от числа 10, нужно выбрать число и знак -, в качестве аргументов'''
+    if symbol == '-':
+        num_subst = num - 10
+        return num_subst
+    else:
+        print('ой')
+
+
+item = calc_add(50, '+')
 print(item)
